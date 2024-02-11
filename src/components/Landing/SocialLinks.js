42a6bcb04661a8c537379a15a4069c15b6bebad3
DESKTOP-1J0EJIQ\waleed <waleed2pac92@gmail.com>
2024-02-11 18:46:13 +0300

    initial project, created landing page

---
+++ src/components/Landing/SocialLinks.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const SocialLinks = ()=>{
+
+
+    return(
+        <div className="flex h-14 gap-10 text-zinc-400">
+            <button>Github</button>
+            <button>LinkedIn</button>
+            <button>Instagram</button>
+            <button>X</button>
+        </div>
+    )
+}
+
+export default SocialLinks;
---
+++ src/components/Landing/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import SocialLinks from "./SocialLinks";
+
+const Landing = () => {
+
+    return (
+        <div className="flex flex-col h-dvh justify-between text-white">
+
+            
+        <div className=" flex flex-auto flex-row justify-between my-40">
+            <div className="flex flex-1 flex-col self-start text-left gap-5">
+                <p className="text-white">Hello My Name is</p>
+                <p className="text-white text-3xl font-bold">Muhammad Waleed Arshad</p>
+                <p className="text-white">I's full stack software enginner - Mobile & web</p>
+                <p className="text-white">Learn More.!</p>
+            </div>
+            <div className="flex flex-1 justify-center">
+                <img className="w-96 h-96 rounded-full" src="https://itswaleed.com/assets/img/1644059050048.jpg"/>
+            </div>
+
+        </div>
+
+        <SocialLinks/>        
+            
+        </div>
+
+
+    )
+}
+
+
+
+export default Landing;
