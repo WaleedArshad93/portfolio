415e08f0c7625fbd48a03f7432a10bc7b9bdeed3
DESKTOP-1J0EJIQ\waleed <waleed2pac92@gmail.com>
2024-02-13 12:05:07 +0300

    Added About me page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App bg-zinc-800 px-40">
       <Landing/>
-
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+const AboutMe = () => {
+
+    return (
+        <div className="flex flex-col h-dvh justify-between text-white">
+
+            
+        <div className=" flex flex-auto flex-row justify-between my-40">
+            <div className="flex basis-1/3 flex-col self-start text-left gap-5">
+                <p className="text-white text-3xl font-bold">About Me</p>
+                <p className="text-white">Full-Stack Engineer experienced in developing and deploying Web and Mobile Applications. Successfully developed and launched mobile applications for various industries, including food delivery as well as mobile apps for utility services. Committed to continuous learning and staying updated with industry trends. Skilled in both collaborative teamwork and independent project management.</p>
+            </div>
+            <div className="flex flex-1 justify-center">
+                <p>
+                    
+                </p>
+            </div>
+
+        </div>
+
+        </div>
+
+
+    )
+}
+
+
+
+export default AboutMe;
