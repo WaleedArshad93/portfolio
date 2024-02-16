8172385abac6d446d0e51968fb14e7dc54553585
DESKTOP-1J0EJIQ\waleed <waleed2pac92@gmail.com>
2024-02-16 15:48:54 +0300

    Added tailwind colors

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
             
         <div className=" flex flex-auto flex-row justify-between my-40">
             <div className="flex basis-1/3 flex-col self-start text-left gap-5">
-                <p className="text-white text-3xl font-bold">About Me</p>
+                <p className="text-primary text-2xl font-bold">About Me</p>
                 <p className="text-white">Full-Stack Engineer experienced in developing and deploying Web and Mobile Applications. Successfully developed and launched mobile applications for various industries, including food delivery as well as mobile apps for utility services. Committed to continuous learning and staying updated with industry trends. Skilled in both collaborative teamwork and independent project management.</p>
             </div>
             <div className="flex flex-1 justify-center">
